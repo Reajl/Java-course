44c5b41ad115811440de60e6646184c5ddb5b25e
Reajl <reajl@bk.ru>
2021-03-04 23:45:08 +0300

    Title

--- Java-course-5/src/com/company/Main.java
+++ Java-course-5/src/com/company/Main.java
@@ -3,7 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String string = "ILoveJavaProgramming";
+        printSubstrings(string);
+
+
+        String string2 = new String("ILoveJavaProgramming");
+        printSubstrings(string2);
     }
-    static public
+
+    static public void printSubstrings(String string) {
+        System.out.print(string.charAt(0));
+        for (int i = 1; i < string.length(); i++) {
+
+
+            if (string.charAt(i) > 'A' && string.charAt(i) < 'Z') {
+                System.out.print("\n" + string.charAt(i));
+            } else System.out.print(string.charAt(i));
+        }
+        System.out.println();
+        System.out.println();
+
+    }
 }
