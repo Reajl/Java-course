b0d034bf61e57ed535dbc0a770f0174de4a63ccf
Reajl <reajl@bk.ru>
2021-03-04 23:25:02 +0300

    Title

--- Java-course-3/src/com/company/Main.java
+++ Java-course-3/src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = {7, 5, 1, 2, 3, 7, 4, 5, 5, 6, 6, 6, 7, 7};
-        findCountMax(array);
+       System.out.println( findCountMax(array));
 
     }
 
-    static private void findCountMax(int[] array) {
+    static private int findCountMax(int[] array) {
         int max = 0;
         int count = 0;
         for (int i : array
@@ -23,6 +23,6 @@ public class Main {
 
 
         }
-        System.out.println(count);
+       return count;
     }
 }
